cd506452d7ca5a0074adf19642453501d8b7bb40
Jiaqi <jiaqiviolin@gmail.com>
2026-09-10 22:39:03 -0600

    1: get usermood. 2: API fetch from GIPHY works

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 
 const moodButton = document.querySelector("#mood-button");
 const moodInput = document.querySelector(".mood-input");
+const moodGif = document.querySelector("#mood-gif");
 
 
 moodButton.addEventListener("click", async function() {
     const userMood = moodInput.value.trim().toLowerCase();
-    const response = await fetch("../data.json")                       // fetch raw data from data.json
-    const data = await response.json()                                  // turn raw data result into js object i can use
+    const response = await fetch("data.json");                        // fetch raw data from data.json
+    const data = await response.json();                                  // turn raw data result into js object i can use
 
-    let opposite = data.oppositeMood[userMood]
+    let opposite = data.oppositeMood[userMood];
     if(! opposite){
-        opposite = data.oppositeMood["slap"]
-    }
+        opposite = data.oppositeMood["slap"];
+    };
     console.log(opposite);  
+
+    const gifResponse = await fetch(
+        `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${encodeURIComponent(opposite)}&limit=1`
+    );
+    const gifData = await gifResponse.json();
+    const gifUrl = gifData.data[0].images.original.url;
+    moodGif.src = gifUrl;   // show the gif on the page
 })
+
