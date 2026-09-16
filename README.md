0364b9fef6831825ef96d96557b493d904ac1cf3
Jiaqi <jiaqiviolin@gmail.com>
2026-09-16 13:49:51 -0600

    generate salad part done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Random-Generation
+
+<a href="https://www.flaticon.com/free-icons/fight" title="fight icons">Fight icons created by Eucalyp - Flaticon</a>
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-const moodButton = document.querySelector("#mood-button");
+const moodInputButton = document.querySelector("#button-hitMe");
 const moodInput = document.querySelector(".mood-input");
 const moodGif = document.querySelector("#mood-gif");
 
 
-moodButton.addEventListener("click", async function() {
+moodInputButton.addEventListener("click", async function() {
     const userMood = moodInput.value.trim().toLowerCase();
     const response = await fetch("data.json");                        // fetch raw data from data.json
     const data = await response.json();                                  // turn raw data result into js object i can use
@@ -16,10 +16,49 @@ moodButton.addEventListener("click", async function() {
     console.log(opposite);  
 
     const gifResponse = await fetch(
-        `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${encodeURIComponent(opposite)}&limit=1`
+        `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${encodeURIComponent(opposite)}&limit=25`
     );
     const gifData = await gifResponse.json();
-    const gifUrl = gifData.data[0].images.original.url;
-    moodGif.src = gifUrl;   // show the gif on the page
+    const randomIndex = Math.floor(Math.random() * gifData.data.length);
+    const gifUrl = gifData.data[randomIndex].images.original.url;
+    moodGif.src = gifUrl; 
 })
 
+
+const makeSaladButton = document.querySelector("#button-makeSalad");
+const saladResult = document.querySelector("#salad-result")
+
+function randomPick(saladArr){
+    return saladArr[Math.floor(Math.random() * saladArr.length)]    
+}
+
+makeSaladButton.addEventListener("click", async function(){
+    const protein = document.querySelector("#protein").value.trim().toLowerCase();
+    const greens = document.querySelector("#greens").value.trim().toLowerCase();
+    const dressing = document.querySelector("#dressing").value.trim().toLowerCase();
+
+    const response = await fetch("data.json");          
+    const data = await response.json();                 
+    const salad = data.makeSalad;
+
+    const topping = randomPick(salad.toppings);
+    const sauce = randomPick(salad.sauces);
+    const liquid = randomPick(salad.liquids);
+    const whatever = randomPick(salad.whatevers);
+    const oil = randomPick(salad.oils);
+
+    saladResult.textContent = 
+    `Your Mystery Salad:
+    -------------------
+    Greens:    ${greens}
+    Protein:   ${protein}
+    Dressing:  ${dressing}
+    Topping:   ${topping}
+    Sauce:     ${sauce}
+    Liquid:    ${liquid}
+    Whatever:  ${whatever}
+    Oil:       ${oil};\n
+    
+    And, voilà! you got your salad!
+    have a miserable day!`;
+})
